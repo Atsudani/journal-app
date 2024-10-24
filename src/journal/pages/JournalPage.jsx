5380fdacc8d021bc71a8c61983d8e302aa89c2ec
atsudani <atsudani@gmail.com>
2024-10-24 14:54:18 -0300

    Primer commit

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
+
+export const JournalPage = () => {
+    return (
+        <JournalLayout>
+            <Typography>Aliquip ex in ad eiusmod minim ipsum incididunt et. Aliquip cillum dolore qui consectetur ea officia duis sunt dolore mollit et aliquip elit. Sunt tempor amet veniam elit dolore aliqua nisi elit. Ex culpa esse mollit cupidatat. Ad exercitation eu deserunt minim pariatur est in mollit aute consequat cupidatat laborum. In reprehenderit enim consequat labore. Magna aliquip sunt Lorem quis nostrud.</Typography>
+
+        </JournalLayout>
+    )
+}
